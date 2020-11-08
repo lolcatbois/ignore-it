c3abf5a0d8787341bb2d74348c4650489002def0
Zerebratox <felix.macho@gmx.at>
2020-11-09 00:57:54 +0100

    ADD adding flag (with this its also chainable)

--- lib/ignore_it.rb
+++ lib/ignore_it.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require 'net/http'
 require 'json'
 require 'colorize'
@@ -16,17 +17,26 @@ module IgnoreIt
     end
 
     def start
-
       ARGV << '-h' if ARGV.empty?
 
       OptionParser.new do |parser|
         parser.banner = "How to Use ignore-it: Pass one of the following options: e.g => ignore-it -f csharp"
-        parser.on(
-          "-f ", "--file FILE", "Select gitignore template to fetch"
-        ) do |file|
+        parser.on("-f ", "--file FILE", "Select gitignore template to fetch") do |file|
           file = file.downcase
+          # puts "Arguments: " + file
           if @list.check_list(file)
             @creator.create_ignore(file)
+          else
+            puts "The template you tried to fetch does not exist".colorize(:red)
+            puts "Please checkout the available templates with " + "ignore-it -l".colorize(:green)
+          end
+          # @options[:file] = true
+        end
+        parser.on("-a ", "--add FILE", "Select gitignore template to add to a existing .gitignore file") do |file|
+          file = file.downcase
+          # puts "Arguments: " + file
+          if @list.check_list(file)
+            @creator.add_ignore(file)
           else
             puts "The template you tried to fetch does not exist".colorize(:red)
             puts "Please checkout the available templates with " + "ignore-it -l".colorize(:green)
